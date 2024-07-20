e6de3d4f4575c09688ba2ab377dc2fdeaab3fed6
Ali HAMMADI GHANEM <ali.ghanem@gmail.com>
2024-07-20 12:31:57 +0200

    Get the take date to move images to folder instead of the modified date

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/stretchr/testify v1.9.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/h2non/filetype v1.1.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,32 @@ import (
 	"io/fs"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/h2non/filetype"
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
 )
 
+func init() {
+	// Optionally register camera makenote data parsing - currently Nikon and
+	// Canon are supported.
+	exif.RegisterParsers(mknote.All...)
+}
+
 func main() {
+
+	dryRunValue := os.Getenv(("DRY_RUN"))
+	dryRun := true
+	if dryRunValue != "" {
+		v, err := strconv.ParseBool(dryRunValue)
+		if err != nil {
+			log.Fatal("dry run value invalid", "dry_run", dryRunValue)
+		}
+		dryRun = v
+	}
 
 	// This folder is the folder where are located the images we want to move
 	sourceFolderPath := os.Getenv("PICTURES_FOLDER")
@@ -18,7 +38,7 @@ func main() {
 	}
 
 	// use path join to format correctly the source folder path
-	sourceFolderPath = path.Join(sourceFolderPath)
+	sourceFolderPath = filepath.Join(sourceFolderPath)
 
 	// Read the folder to get all files in this folder
 	entries, err := os.ReadDir(sourceFolderPath)
@@ -49,15 +69,36 @@ func main() {
 	// Get the parent folder of the folder source
 	// For example if the folder source is C:/images/pellicule
 	// the parent folder will be C:/images
-	rootPath := path.Dir(sourceFolderPath)
+	rootPath := filepath.Dir(sourceFolderPath)
 	i := 0
 
 	for _, file := range files {
+		if file.Name() == "desktop.ini" {
+			// TODO improve it by allowing only certain file types
+			// ignore it
+			continue
+		}
+
+		oldPath := filepath.Join(sourceFolderPath, file.Name())
 
 		// Compute the destination folder of the image based on its modification date
 		// The destination folder will be like C:/images/2024/2024-06-15
-		destinationFolder := computeDestinationFolder(rootPath, file.ModTime())
-		_, err := os.Stat(destinationFolder)
+		takenTime, err := getTakenTime(oldPath, file)
+		if err != nil {
+			log.Println("failed to get taken time", "path", oldPath, "err", err)
+			continue
+		}
+
+		// Compute destination folder and the complete new path of the file
+		destinationFolder := computeDestinationFolder(rootPath, takenTime)
+		newPath := filepath.Join(destinationFolder, file.Name())
+
+		if dryRun {
+			log.Println("dry run", "old_path", oldPath, "new_path", newPath, "date", takenTime)
+			continue
+		}
+
+		_, err = os.Stat(destinationFolder)
 		if os.IsNotExist(err) {
 			// Create the destination folder if it does not existy
 			err = os.Mkdir(destinationFolder, 0755)
@@ -65,9 +106,6 @@ func main() {
 				log.Fatalln("creating image folder", "folder_path", destinationFolder)
 			}
 		}
-
-		oldPath := path.Join(sourceFolderPath, file.Name())
-		newPath := path.Join(destinationFolder, file.Name())
 
 		err = os.Rename(oldPath, newPath)
 		if err != nil {
@@ -87,8 +125,49 @@ func main() {
 }
 
 func computeDestinationFolder(imagesFolderPath string, fileDate time.Time) string {
-	yearFolder := path.Join(imagesFolderPath, strconv.Itoa(fileDate.Year()))
+	yearFolder := filepath.Join(imagesFolderPath, strconv.Itoa(fileDate.Year()))
 	folderName := fileDate.Format("2006-01-02")
-	folderPath := path.Join(yearFolder, folderName)
+	folderPath := filepath.Join(yearFolder, folderName)
 	return folderPath
+}
+
+func getTakenTime(filePath string, fileInfo fs.FileInfo) (time.Time, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer f.Close()
+
+	// We only have to pass the file header = first 261 bytes
+	head := make([]byte, 261)
+	f.Read(head)
+
+	if !filetype.IsImage(head) {
+		return fileInfo.ModTime(), nil
+	}
+
+	// reset the reader to allow exif read the file
+	f.Seek(0, 0)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		if err.Error() == "EOF" {
+			// Cannot decode exit maybe does not exist in the file
+			// Returns the mod type
+			return fileInfo.ModTime(), nil
+		}
+
+		return time.Time{}, err
+	}
+
+	date, err := x.DateTime()
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	if !date.IsZero() {
+		return date, nil
+	}
+
+	return fileInfo.ModTime(), nil
 }
